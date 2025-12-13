0d5f0ff999a8e0dd77eedec162e9f15c95246e07
Fai <alkhf824@newschool.edu>
2025-12-13 12:56:41 -0500

    UPDATE

---
+++ MazeGame1/Assets/script 1/SpikeSideMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SpikeSideMove : MonoBehaviour
+{
+    public float moveDistance = 2f;
+    public float speed = 2f;
+
+    private int direction = 1;
+    private float startX;
+
+    void Start()
+    {
+        startX = transform.position.x;
+    }
+
+    void Update()
+    {
+        if (transform.position.x >= startX + moveDistance)
+            direction = -1;
+        else if (transform.position.x <= startX - moveDistance)
+            direction = 1;
+
+        transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
+    }
+}
